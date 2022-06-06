7f90d1555fda8ba02e72f685d2ae093fbc05cbd5
hawkprimarch <hawkprimarch@mail.ru>
2022-06-06 15:14:38 +0500

    5 step fixes

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,7 +1,9 @@
+import yaml from 'js-yaml';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 import * as path from 'path';
 import { dirname } from 'path';
-import { getNormalizePath, getDiff } from '../src/getDiff.js';
+import { getDiff } from '../src/getDiff.js';
 
 const trullyMass = [
   { name: 'follow', oldValue: false },
@@ -17,7 +19,10 @@ const __dirname = dirname(__filename);
 const getFixture = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('getDiff', () => {
-  const obj1 = getNormalizePath(getFixture('filepath1.json'));
-  const obj2 = getNormalizePath(getFixture('filepath2.json'));
+  const obj1 = JSON.parse((fs.readFileSync(getFixture('file1.json'), 'utf-8')));
+  const obj2 = JSON.parse((fs.readFileSync(getFixture('file2.json'), 'utf-8')));
   expect(getDiff(obj1, obj2)).toEqual(trullyMass);
+  const obj3 = yaml.load((fs.readFileSync(getFixture('file3.yml'), 'utf-8')));
+  const obj4 = yaml.load((fs.readFileSync(getFixture('file4.yml'), 'utf-8')));
+  expect(getDiff(obj3, obj4)).toEqual(trullyMass);
 });
