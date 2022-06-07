3e7fe46e8ecce9c309f8ff38962e4f1dc851f492
hawkprimarch <hawkprimarch@mail.ru>
2022-06-07 17:56:18 +0500

    six step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,9 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-V, --version', 'output the version number', '0.0.1')
-  .action((filepath1, filepath2) => {
-    getGeneralLogic(filepath1, filepath2);
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2, { format }) => {
+    getGeneralLogic(filepath1, filepath2, format);
   })
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format');
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import fs from 'fs';
-import { getNormalizePath, getDiff, printAnswer } from './getDiff.js';
+
+import { getNormalizePath, getDiff } from './getDiff.js';
+import stylish from './stylish.js';
 import getParser from './parsers.js';
 
-const getGeneralLogic = (path1, path2) => {
+const getGeneralLogic = (path1, path2, format = 'stylish') => {
   const normalizePath1 = getNormalizePath(path1);
   const normalizePath2 = getNormalizePath(path2);
 
@@ -12,7 +14,9 @@ const getGeneralLogic = (path1, path2) => {
   const obj1 = parserFromFile1(fs.readFileSync(normalizePath1, 'utf-8'));
   const obj2 = parserFromFile2(fs.readFileSync(normalizePath2, 'utf-8'));
   const result = getDiff(obj1, obj2);
-  printAnswer(result);
+  if (format === 'stylish') {
+    stylish(result);
+  }
 };
 
 export default getGeneralLogic;
